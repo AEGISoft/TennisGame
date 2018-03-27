a9a81ee2b82b886347bb0a077adf855f8ddef1ed
Olivier.Costa <Olivier.Costa@AEGISoft.be>
2018-03-27 18:02:40 +0200

    move extracted class to it's own file

---
+++ Tennis/Player.cs
@@ -0,0 +1,23 @@
+﻿namespace Tennis
+{
+    internal class Player
+    {
+        #region construction
+        public string Name { get; }
+        public Player(string playerName)
+        {
+            this.Name = playerName;
+            this.Score = 0;
+        }
+        #endregion
+
+        #region published interface
+        public int Score { get; private set; }
+
+        public void ScoredPointByWinningRally()
+        {
+            Score++;
+        }
+        #endregion
+    }
+}
--- Tennis/TennisGame.cs
+++ Tennis/TennisGame.cs
@@ -3,26 +3,6 @@ using System;
 
 namespace Tennis
 {
-    internal class Player
-    {
-        #region construction
-        public string Name { get; }
-        public Player(string playerName)
-        {
-            this.Name = playerName;
-            this.Score = 0;
-        }
-        #endregion
-
-        #region published interface
-        public int Score { get; private set; }
-
-        public void ScoredPointByWinningRally()
-        {
-            Score++;
-        }
-        #endregion
-    }
     public class TennisGame
     {
 
