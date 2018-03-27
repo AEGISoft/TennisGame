8b8287074c2d0a90a3a562cda8a32c0d994646f9
Olivier.Costa <Olivier.Costa@AEGISoft.be>
2018-03-27 19:05:52 +0200

    to much responsibilities : treat with extract method to class

--- Tennis.Test/VerifyTennisGameScore.cs
+++ Tennis.Test/VerifyTennisGameScore.cs
@@ -5,19 +5,6 @@ namespace Tennis.Test
 {
     public class VerifyTennisGameScore
     {
-        [Fact]
-        public void ExampleGame()
-        {
-            TennisGame game = new TennisGame("player1", "player2");
-            string[] points = { "player1", "player1", "player2", "player2", "player1", "player1" };
-            string[] expected_scores = { "Fifteen-Love", "Thirty-Love", "Thirty-Fifteen", "Thirty-All", "Forty-Thirty", "Win for Player 1" };
-
-            for (int i = 0; i < points.Length; i++)
-            {
-                game.WonPoint(points[i]);
-                Assert.Equal(expected_scores[i], game.GetScore());
-            }
-        }
 
         [Theory]
         [InlineData(0, 0, "Love-All")]
---
+++ Tennis.Test/WhenFollowingAnExampleGame.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace Tennis.Test
+{
+    public class WhenFollowingAnExampleGame
+    {
+        [Fact]
+        public void TheGameScoreShouldReflectTheScoresOfEachRally()
+        {
+            TennisGame game = new TennisGame("player1", "player2");
+            string[] points = { "player1", "player1", "player2", "player2", "player1", "player1" };
+            string[] expected_scores = { "Fifteen-Love", "Thirty-Love", "Thirty-Fifteen", "Thirty-All", "Forty-Thirty", "Win for Player 1" };
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                game.WonPoint(points[i]);
+                Assert.Equal(expected_scores[i], game.GetScore());
+            }
+        }
+    }
+}
